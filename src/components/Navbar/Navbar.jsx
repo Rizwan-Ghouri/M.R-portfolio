8d3e715f3433de2d15807061eefc07db207b1dad
Rizwan-Ghouri <RizwanIdrees138@gmail.com>
2025-12-31 22:45:46 +0500

    simple change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
-import { BiSolidMoon, BiSolidSun } from "react-icons/bi";
+import { BiMenu, BiSolidMoon, BiSolidSun } from "react-icons/bi";
 import { Navlinks } from "../../utils/constants/Navlink";
 import PropTypes from "prop-types";
 import Heading from "../common/Heading/Heading";
 
 const Navbar = ({ theme, setTheme }) => {
+  
   return (
     <nav className="container page-center bg-textcolor text-darkcolor dark:bg-darkcolor dark:text-textcolor shadow-md shadow-darkcolor dark:shadow-white px-4 py-1 sticky top-0 inset-x-0 z-50">
       <div className="flex items-center justify-between py-2 md:px-5">
@@ -42,17 +43,23 @@ const Navbar = ({ theme, setTheme }) => {
           </ul>
         </div>
         {/* dark mode btn */}
-        <div className="md:hidden">
+        <div className="md:hidden flex justify-between">
           {theme === "dark" ? (
-            <BiSolidSun
-              className="text-2xl cursor-pointer"
-              onClick={() => setTheme("light")}
-            />
+            <div className="flex gap-2 items-center">
+              <BiSolidSun
+                className="text-2xl cursor-pointer"
+                onClick={() => setTheme("light")}
+              />
+              <BiMenu className="text-3xl cursor-pointer md:hidden" />
+            </div>
           ) : (
-            <BiSolidMoon
-              className="text-2xl cursor-pointer"
-              onClick={() => setTheme("dark")}
-            />
+            <div className="flex gap-2 items-center">
+              <BiSolidMoon
+                className="text-2xl cursor-pointer"
+                onClick={() => setTheme("dark")}
+              />
+              <BiMenu className="text-3xl cursor-pointer md:hidden" />
+            </div>
           )}
         </div>
       </div>
