88311a20ad7da31304984e85f3d6fc802df0915f
Rizwan-Ghouri <RizwanIdrees138@gmail.com>
2026-01-11 12:27:04 +0500

    slide remove

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -2,88 +2,26 @@ import React from "react";
 import Heading from "../common/Heading/Heading";
 import skillsCards from "../../utils/constants/SkillsCards";
 import SkillsCards from "../common/SkillsCards/SkillsCards";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 const Skills = () => {
-  function SampleNextArrow() {
-    return null; // kuch render hi nahi hoga
-  }
-  function SamplePrevArrow() {
-    return null; // kuch render hi nahi hoga
-  }
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    initialSlide: 2,
-    autoplay: true,
-    nextArrow: <SampleNextArrow/>,
-    prevArrow: <SamplePrevArrow/>,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-          dots: false,
-
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 2,
-
-          dots: true,
-        },
-      },
-    ],
-  };
   return (
     <div
       id="skills"
-      className="text-darkcolor dark:bg-darkcolor dark:text-textcolor p-5 pt-15 h-100"
+      className="text-darkcolor dark:bg-darkcolor dark:text-textcolor p-5 pt-15 h-full h-0-screen         flex flex-col justify-center items-center"
     >
-      <div >
+      <div>
         <div className="flex items-center justify-center">
           <Heading
             headingClass="text-4xl py-5 font-semibold font-serif duration-300"
             headingName="My Skills"
           />
         </div>
-        {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6"> */}
-        <div className="slider-container">
-          <Slider {...settings}>
-            {skillsCards.map((data) => {
-              const { id, title, description, level, icon } = data;
-              return (
-                <div className=" grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3" key={id}>
-                  <SkillsCards
-                    icon={icon}
-                    title={title}
-                    description={description}
-                    level={level}
-                  />
-                </div>
-              );
-            })}
-          </Slider>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {skillsCards.map((data, index) => (
+            <div key={index}>
+              <SkillsCards {...data} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
--- src/components/common/SkillsCards/SkillsCards.jsx
+++ src/components/common/SkillsCards/SkillsCards.jsx
@@ -5,12 +5,9 @@ const SkillsCards = ({ title, description, level, icon }) => {
   const Icon = icon;
 
   return (
-    <div
-      // data-aos="fade-up"
-      className="text-darkcolor dark:bg-darkcolor dark:text-textcolor rounded-2xl hover:border-2 dark:hover:border-bluecolor hover:border-Dhover duration-300 m-2"
-    >
-      <div className="p-5 shadow-darkcolor hover:duration-500 dark:shadow-textcolor rounded-2xl shadow-md ">
-        <div className="flex items-center gap-2">
+    <div data-aos="fade-up" className="text-darkcolor dark:bg-darkcolor dark:text-textcolor rounded-2xl hover:border-2 hover:border-bluecolor duration-300">
+      <div className="shadow-darkcolor hover:duration-500 dark:shadow-textcolor rounded-2xl shadow-lg p-5">
+        <div className="flex items-center gap-4">
           <span>{<Icon className="text-3xl" />}</span>
           <Heading
             headingClass="text-3xl font-semibold font-serif duration-300"
@@ -21,7 +18,7 @@ const SkillsCards = ({ title, description, level, icon }) => {
           <h2 className="font-semibold ">
             level : <span className="font-normal">{level}</span>
           </h2>
-          <p className="font-semibold scroll-m-1">
+          <p className="font-semibold ">
             Description : <span className="font-normal">{description}</span>
           </p>
         </div>
